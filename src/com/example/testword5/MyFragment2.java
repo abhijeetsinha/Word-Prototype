1ec7b9b0360464dffcc06ccc4e246bb73f11c3d1
abhijeet <abhijeetsinha027@gmail.com>
2014-03-13 17:55:39 +0530

    Project added to github

---
+++ src/com/example/testword5/MyFragment2.java
@@ -0,0 +1,181 @@
+package com.example.testword5;
+
+import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.app.ListFragment;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MyFragment2 extends Activity {
+
+	private static final String TITLES[]={"Back","New", "Open","Make a copy","History","Settings"};
+	private static final String INFO[]={"This is NEW fragment","This is OPEN fragment",
+		"This is MAKE A COPY fragment","This is HISTORY fragment","This is SETTINGS fragment"};
+	static Activity activity;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		activity=this;
+		ActionBar actionBar=getActionBar();
+		actionBar.hide();
+		setContentView(R.layout.fragment_layout);
+	}
+
+	
+	public static class DetailsActivity extends Activity{
+		
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+
+			if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+				finish();
+				return;
+			}
+			if (savedInstanceState == null) {
+				DetailsFragment details = new DetailsFragment();
+				details.setArguments(getIntent().getExtras());
+				getFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
+			}
+		}
+	}
+	
+	public static class TitlesFragment extends ListFragment{
+		boolean mDualPane;
+		int mCurCheckPosition = 1;
+		
+		@Override
+		public void onActivityCreated(Bundle savedInstanceState) {
+			super.onActivityCreated(savedInstanceState);
+			
+			setListAdapter(new ArrayAdapter<String>(getActivity(),
+					android.R.layout.simple_list_item_activated_1,
+					TITLES));
+			
+			View detailsFrame = getActivity().findViewById(R.id.details);
+			mDualPane = detailsFrame != null && detailsFrame.getVisibility() == View.VISIBLE;
+
+			if (savedInstanceState != null) {
+				// Restore last state for checked position.
+				mCurCheckPosition = savedInstanceState.getInt("curChoice", 1);
+			}
+			
+			if (mDualPane) {
+				getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+				showDetails(mCurCheckPosition);
+			} 
+			
+			else {
+				getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+				getListView().setItemChecked(mCurCheckPosition, true);
+				//getListView().setBackgroundColor("#357ec7");
+			}
+		}
+		
+	
+		/*public View getView(int position, View convertView, ViewGroup parent){
+			View v = super.getView(position,convertView,parent);
+			TextView text=(TextView)v.findViewById(android.R.id.text1);
+			text.setTextColor(Color.WHITE);
+			return v;
+		}*/
+		
+		@Override
+		public void onViewCreated(View v, Bundle savedInstanceState){
+			super.onViewCreated(v, savedInstanceState);
+			ListView l=getListView();
+			l.setBackgroundColor(Color.parseColor("#357ec7"));
+		}
+		
+		@Override
+		public void onSaveInstanceState(Bundle outState) {
+			super.onSaveInstanceState(outState);
+			outState.putInt("curChoice", mCurCheckPosition);
+		}
+		
+		@Override
+		public void onListItemClick(ListView l, View v, int position, long id) {
+			showDetails(position);
+		}
+		
+		void showDetails(int index) {
+			
+			if(index==0)
+				activity.finish();
+			else{
+				mCurCheckPosition = index;
+				
+				if (mDualPane) {
+					getListView().setItemChecked(index, true);
+					DetailsFragment details = (DetailsFragment) getFragmentManager().findFragmentById(R.id.details);
+					
+					if (details == null || details.getShownIndex() != index) {
+						details = DetailsFragment.newInstance(index);
+
+						FragmentTransaction ft = getFragmentManager().beginTransaction();
+						ft.replace(R.id.details, details);
+						ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+						ft.commit();
+					}
+
+				} else {
+
+					Intent intent = new Intent();
+					intent.setClass(getActivity(), DetailsActivity.class);
+					intent.putExtra("index", index);
+					startActivity(intent);
+				}
+			}
+			
+		}
+		
+	}
+	
+	public static class DetailsFragment extends Fragment{
+		
+		public static DetailsFragment newInstance(int index) {
+			DetailsFragment f = new DetailsFragment();
+
+			Bundle args = new Bundle();
+			args.putInt("index", index);
+			f.setArguments(args);
+
+			return f;	
+		}
+
+		public int getShownIndex() {
+			return getArguments().getInt("index", 0);
+		}
+		
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+
+			ScrollView scroller = new ScrollView(getActivity());
+			TextView text = new TextView(getActivity());
+			int padding = (int) TypedValue.applyDimension(
+					TypedValue.COMPLEX_UNIT_DIP, 4, getActivity()
+							.getResources().getDisplayMetrics());
+			text.setPadding(padding, padding, padding, padding);
+			scroller.addView(text);
+			text.setText(INFO[getShownIndex()-1]);
+			return scroller;
+		}
+	}
+
+}
---
+++ src/com/example/testword5/TestWord5.java
@@ -0,0 +1,253 @@
+package com.example.testword5;
+
+import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ActionBar.LayoutParams;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+
+public class TestWord5 extends Activity {
+
+	private Spinner fontface;
+	private Spinner fontsize;
+	private Spinner textStyles;
+	private Button file;
+	private Button home;
+	private Button insert;
+	private Button layout;
+	private Button review;
+	private Button view;
+	
+	private ImageButton keyboardlock;
+	private ImageButton keyboardunlock;
+	private ImageView keyboard;
+	
+	private Intent i;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		ActionBar actionBar=getActionBar();
+		actionBar.hide();
+		setContentView(R.layout.activity_test_word5);
+		
+		file=(Button)findViewById(R.id.button_file);
+		home=(Button)findViewById(R.id.button_home);
+		insert=(Button)findViewById(R.id.button_insert);
+		layout=(Button)findViewById(R.id.button_layout);
+		review=(Button)findViewById(R.id.button_review);
+		view=(Button)findViewById(R.id.button_view);
+		
+		home.setBackgroundColor(0x0000);
+		
+		fontface=(Spinner)findViewById(R.id.spinner_fontface);
+		final ArrayAdapter<CharSequence> adapter_fontface=ArrayAdapter.createFromResource(this,R.array.spinner_fontface,android.R.layout.simple_spinner_item);
+		adapter_fontface.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		fontface.setAdapter(adapter_fontface);
+		
+		fontsize=(Spinner)findViewById(R.id.spinner_fontsize);
+		final ArrayAdapter<CharSequence> adapter_fontsize=ArrayAdapter.createFromResource(this,R.array.spinner_fontsize,android.R.layout.simple_spinner_item);
+		adapter_fontsize.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		fontsize.setAdapter(adapter_fontsize);
+		
+		textStyles=(Spinner)findViewById(R.id.spinner_textstyles);
+		final ArrayAdapter<CharSequence> adapter_textstyles=ArrayAdapter.createFromResource(this,R.array.spinner_textstyles,android.R.layout.simple_spinner_item);
+		adapter_textstyles.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		textStyles.setAdapter(adapter_textstyles);
+	
+		file.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				/*AlertDialog.Builder builder = new AlertDialog.Builder(TestWord5.this);
+				LayoutInflater inflater = TestWord5.this.getLayoutInflater();
+				builder.setView(inflater.inflate(R.layout.backstage, null));     
+				builder.show();*/
+				//FragmentManager fragmentManager = getFragmentManager();
+				//CustomDialogFragment newFragment= new CustomDialogFragment();
+				
+				i=new Intent(TestWord5.this,MyFragment2.class);
+				//i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+				//startActivityForResult(i,Intent.FLAG_ACTIVITY_SINGLE_TOP);
+				startActivity(i);
+				
+			}
+			
+		});
+		
+		home.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+	            LinearLayout ribbonLayout = (LinearLayout)findViewById(R.id.ribbon_entry);
+	            ribbonLayout.removeAllViews();
+	 
+	            LayoutInflater inflater = getLayoutInflater();
+	            ribbonLayout.addView(inflater.inflate(R.layout.ribbon_home, null));
+	            home.setBackgroundColor(0x0000);
+	            insert.setBackgroundColor(Color.LTGRAY);
+	            layout.setBackgroundColor(Color.LTGRAY);
+	            review.setBackgroundColor(Color.LTGRAY);
+	            view.setBackgroundColor(Color.LTGRAY);
+	            
+			}
+			
+		});
+		
+		insert.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+	            LinearLayout ribbonLayout = (LinearLayout)findViewById(R.id.ribbon_entry);
+	            ribbonLayout.removeAllViews();
+	 
+	            LayoutInflater inflater = getLayoutInflater();
+	            ribbonLayout.addView(inflater.inflate(R.layout.ribbon_insert, null));
+
+	            home.setBackgroundColor(Color.LTGRAY);
+	            insert.setBackgroundColor(0x0000);
+	            layout.setBackgroundColor(Color.LTGRAY);
+	            review.setBackgroundColor(Color.LTGRAY);
+	            view.setBackgroundColor(Color.LTGRAY);
+	            
+			}
+			
+		});
+		
+		layout.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+	            LinearLayout ribbonLayout = (LinearLayout)findViewById(R.id.ribbon_entry);
+	            ribbonLayout.removeAllViews();
+	 
+	            LayoutInflater inflater = getLayoutInflater();
+	            ribbonLayout.addView(inflater.inflate(R.layout.ribbon_layout, null));
+
+	            home.setBackgroundColor(Color.LTGRAY);
+	            insert.setBackgroundColor(Color.LTGRAY);
+	            layout.setBackgroundColor(0x0000);
+	            review.setBackgroundColor(Color.LTGRAY);
+	            view.setBackgroundColor(Color.LTGRAY);
+	            
+			}
+			
+		});
+		
+		review.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+	            LinearLayout ribbonLayout = (LinearLayout)findViewById(R.id.ribbon_entry);
+	            ribbonLayout.removeAllViews();
+	 
+	            LayoutInflater inflater = getLayoutInflater();
+	            ribbonLayout.addView(inflater.inflate(R.layout.ribbon_review, null));
+	 
+	            home.setBackgroundColor(Color.LTGRAY);
+	            insert.setBackgroundColor(Color.LTGRAY);
+	            layout.setBackgroundColor(Color.LTGRAY);
+	            review.setBackgroundColor(0x0000);
+	            view.setBackgroundColor(Color.LTGRAY);
+	            
+			}
+			
+		});
+		
+		view.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+	            LinearLayout ribbonLayout = (LinearLayout)findViewById(R.id.ribbon_entry);
+	            ribbonLayout.removeAllViews();
+	 
+	            LayoutInflater inflater = getLayoutInflater();
+	            ribbonLayout.addView(inflater.inflate(R.layout.ribbon_view, null));
+
+	            home.setBackgroundColor(Color.LTGRAY);
+	            insert.setBackgroundColor(Color.LTGRAY);
+	            layout.setBackgroundColor(Color.LTGRAY);
+	            review.setBackgroundColor(Color.LTGRAY);
+	            view.setBackgroundColor(0x0000);
+	       
+			}
+			
+		});
+		
+		keyboardlock=(ImageButton)findViewById(R.id.button_keyboardlock);
+		keyboardunlock=(ImageButton)findViewById(R.id.button_keyboardunlock);
+		keyboard=(ImageView)findViewById(R.id.keyboardicon_up);
+		
+		keyboardunlock.setEnabled(false);
+		
+		keyboardlock.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				int[] coordinates=new int[2];
+				keyboardlock.getLocationOnScreen(coordinates);
+				Log.d("MYTAG","Reached1");
+				System.out.println(coordinates[0]);
+				System.out.println(coordinates[1]);
+				Log.d("MYTAG","Reached2");
+				keyboard.setImageResource(R.drawable.keyboard_lock);
+				RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+				params.setMargins(1730,720,0,0);
+				keyboard.setLayoutParams(params);
+				keyboardlock.setEnabled(false);
+				keyboardunlock.setEnabled(true);
+			}
+			
+		});
+		
+		keyboardunlock.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				keyboard.setImageResource(R.drawable.keyboard_invoke);
+				RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+				params.setMargins(1822,720,0,0);
+				keyboard.setLayoutParams(params);
+				keyboardunlock.setEnabled(false);
+				keyboardlock.setEnabled(true);
+			}
+			
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.test_word5, menu);
+		return true;
+	}
+
+}
